d828e08b65244d65338fcb197232c73932cc638e
Ammar Homsi <ammar-hms@hotmail.com>
2025-10-07 03:04:46 -0700

    Add comments to the ForS chleife.java

--- b_for/ForSchleife.java
+++ b_for/ForSchleife.java
@@ -2,8 +2,20 @@ public class ForSchleife
 {
    public static void main(String[] args)
    {
+       // Der Kopf der ForSchleife steuert die
+       // Anzahl der Wiederholungen.
+
+       // Er hat drei Abschnitte:
+
+       // 1.) Deklaration der Zaehlveriablen i und
+       // Initialisierung mit dem STARTWERT 1.
+       // 2.) Redingung fuer den ENDWERT: i darf nur
+       // kleiner oder gleich 20 sein (nicht grosser)
+       // 3.) Die SCHRITTWEITE fuer das Hochzaehlen der
+       // Variable i 
       for(int i = 1; i <= 20; i++)
       {
+         
          System.out.println("Hallo " + i);
       }
    }
